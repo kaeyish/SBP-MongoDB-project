e9d54792381b403e8f061bb74c3d055aae613e8f
Dusanka Ignjatovic <ignjatovicdusanka02@gmail.com>
2026-06-22 21:03:33 +0200

    Database tweaks and q1v1

--- preview-podataka.py
+++ preview-podataka.py
@@ -7,20 +7,32 @@ Created on Tue May 26 20:36:03 2026
 
 
 """
-1. Za svakog klijenta koji je ušao u default, odrediti mesec pre ulaska u default u kome je imao najveći pad kreditnog skora u prethodna 3 meseca.
-2. Pronaći klijente kod kojih je iskoriscenost kredita porastao više od 40%, a kreditna sposobnost opala za više od 100 poena u periodu od 3 meseca pre ulaska u default status.
-3. Za svaku credit score grupu korisnika odrediti prosecan odnos izmedju odobrene sume kredita i ukupne sume izvrsenih uplata, kao i stopu defaulta.
-4. Za svakog klijenta izračunati najduži uzastopni niz meseci bez propuštene uplate.
-5. Pronaći profil korisnika (kombinacija starosne grupe, pola, zaposlenosti i opsega godišnjih prihoda) koji ima najmanji prosečan DTI i najveći procenat urednih uplata tokom prvih 12 meseci otplate.
-
+1. Za svakog klijenta koji je ušao u default, odrediti mesec pre ulaska u default u kom je imao najveći pad kreditnog rejtinga u prethodna 3 meseca.
+2. Pronaći klijente kod kojih je iskorišćenost kreditne kartice porastao više od 40%, a kreditna sposobnost opala za više od 100 poena u periodu od 3 meseca pre likvidacije.
+3. Za korisnike sa lošim kreditnim rejtingom u trenutku odobrenja kredita (<579) odrediti odnos ukupne sume izvršenih uplata i predviđene vrednosti uplata u prvih 12 meseci, kao i stopu defaulta.
+4. Za korisnike sa izuzetnim kreditnim rejtingom u trenutku odobrenja kredita (>800) pronaći broj korisnika čiji je rejting u nekom trenutku opao u nižu kategoriju (<=799).
+5. Za svakog klijenta izračunati najduži uzastopni niz meseci bez propuštene uplate.
+6. Pronaći profil korisnika (starosnu grupu, rod, status zaposlenosti i opseg godišnjih prihoda) koji ima najmanji prosečan DTI i najveći procenat urednih uplata tokom prvih 12 meseci otplate.
 """
 
+
+
+"""
+Lista ideja za indekse i rekonstruisanje:
+
+- potencijalno smanjenje okvira podataka je i uklanjanje korisnika posle defaultovanja? ili sortiranje po has _defaulted i onda pretraga samo dok je 0 (ili do prvog =1)
+1. index nad customer_id - svakako je foreign key, mada selektivnost je ~17% tkd moze biti i da nije toliko idealan al kao za joinove (doduse nardeni koraci mogu ga prikazati kao suvisnog) 
+2. extended reference na podatke iz origination kolekcije, gender, income itd.
+3. dodavanje calculated polja za 3m razlike i slicne stvari! 
+
+"""
+#%% basic imports
 import pandas as pd
 from pathlib import Path
 import matplotlib.pyplot as plt
 import seaborn as sb
 import numpy as np
-#%% Grafik korelacije
+#%% Target col correlation graph
 
 def plot_correlation_for_col(df, col_name):
     plt.figure(figsize=(12,6)) 
@@ -31,124 +43,8 @@ def plot_correlation_for_col(df, col_name):
     plt.xticks(rotation=90);
     plt.tight_layout()
     plt.show()
-
-#%% POSTAVLJANJE PUTANJA
-base_path = Path(__file__).parent / 'podaci/'
-
-path_credit = base_path / 'credit score/origination_data-2.csv'
-path_credit_tracking = base_path / 'credit score/monthly_performance-2.csv'
-
-print(path_credit ) 
-
-#%% UCITAVANJE PODATAKA
-
-df_credit_score = pd.read_csv(path_credit)
-
-df_performance = pd.read_csv(path_credit_tracking)
-
-#%% FOO ZA UPOZNAVANJE SA PODACIMA
-
-def initial_evaluation(df):
     
-    print ('INFORMACIJE O DATASETU:')
-    print ('-----------------------------------------------------')
-    
-    print ('Oblik:', df.shape)
-    
-    print ('Kolone i tipovi:')
-    
-    print (df.dtypes)
-    
-    print ('Broj NA vrednosti')
-
-    print (np.count_nonzero(df.isna()))    
-    '''
-    print ('Prvih i poslednjih 5 redova:')
-           
-    print (df.head(5))
-    
-    print (df.tail(5))
-    
-    print('Sample data:')
-    
-    print (df.sample(n=5))
-    '''
-#%%
-
-initial_evaluation(df_credit_score) 
-
-#%%
-initial_evaluation(df_performance) 
-
-#%%
-
-print("MONTHLY\n",df_performance.dtypes)
-#%%
-print("ON ORIGIN\n",df_credit_score.dtypes)
-
-#%%
-print (np.unique(df_performance['dpd_current'])) 
-
-
-#%%
-
-
-def backfill_dpd_progression(df):
-
-
-    df = df.sort_values(
-        ["customer_id", "months_on_book"]
-    ).reset_index(drop=True)
-
-
-    for cust_id, idx in df.groupby("customer_id").groups.items():
-
-        customer_rows = df.loc[idx]
-
-        ninety_rows = customer_rows[
-            customer_rows["dpd_current"] >= 90
-        ]
-
-        if ninety_rows.empty:
-            continue
-
-        first_default_idx = ninety_rows.index[0]
-
-        pos = customer_rows.index.get_loc(first_default_idx)
-
-        if pos >= 1:
-            idx_60 = customer_rows.index[pos - 1]
-
-            if df.loc[idx_60, "dpd_current"] == 0:
-                df.loc[idx_60, "dpd_current"] = 60
-
-        if pos >= 2:
-            idx_30 = customer_rows.index[pos - 2]
-
-            if df.loc[idx_30, "dpd_current"] == 0:
-                df.loc[idx_30, "dpd_current"] = 30
-                
-    return df
-
-#%%
-
-df_performance = backfill_dpd_progression(df_performance)
-
-
-print (np.unique(df_performance['dpd_current'])) 
-
-
-#%%
-print (np.unique(df_credit_score['employment_status']))
-    
-    
-#%%plot_correlation_for_col
-
-df_kljucna_polja = df_performance.drop(['customer_id', 'observation_month'], axis = 1)
-
-plot_correlation_for_col(df_kljucna_polja, 'has_defaulted')
-
-#%%
+#%% Correlation matrix
 
 
 def plot_correlation_heatmap(
@@ -185,18 +81,210 @@ def plot_correlation_heatmap(
 
     return corr_matrix
 
-#%%
+#%% Basic data information
+
+def initial_evaluation(df):
+    
+    print ('INFORMACIJE O DATASETU:')
+    print ('-----------------------------------------------------')
+    
+    print ('Oblik:', df.shape)
+    
+    print ('Kolone i tipovi:')
+    
+    print (df.dtypes)
+    
+    print ('Broj NA vrednosti')
+
+    print (np.count_nonzero(df.isna()))    
+
+    print ('Prvih i poslednjih 5 redova:')
+           
+    print (df.head(5))
+    
+    print (df.tail(5))
+    
+    print('Sample data:')
+    
+    print (df.sample(n=5))
+
+
+
+#%% fixing basic dpd inconsistencies
+
+
+def backfill_dpd_progression(df):
+
+
+    df = df.sort_values(
+        ["customer_id", "months_on_book"]
+    ).reset_index(drop=True)
+
+
+    for cust_id, idx in df.groupby("customer_id").groups.items():
+
+        customer_rows = df.loc[idx]
+
+        ninety_rows = customer_rows[
+            customer_rows["dpd_current"] >= 90
+        ]
+
+        if ninety_rows.empty:
+            continue
+
+        first_default_idx = ninety_rows.index[0]
+
+        pos = customer_rows.index.get_loc(first_default_idx)
+
+        if pos >= 1:
+            idx_60 = customer_rows.index[pos - 1]
+
+            if df.loc[idx_60, "dpd_current"] == 0:
+                df.loc[idx_60, "dpd_current"] = 60
+                df.loc[idx_60, "payment_made"] = 0
+                df.loc[idx_60, "payment_amount"] = 0                
+
+        if pos >= 2:
+            idx_30 = customer_rows.index[pos - 2]
+
+            if df.loc[idx_30, "dpd_current"] == 0:
+                df.loc[idx_30, "dpd_current"] = 30
+                df.loc[idx_60, "payment_made"] = 0
+                df.loc[idx_60, "payment_amount"] = 0
+                        
+        later_ninety_idxs = ninety_rows.index[1:]
+        df.loc[later_ninety_idxs, "dpd_current"] = 120
+
+
+    return df
+
+#%% additional dpd tweaks to spice up 5th query
+'''
+NAMERNO ZAMUCIVANJE INACE DOBRIH ISPLATA ZA NEKE OD KORISNIKA
+
+za ~30% klijenata raznih kategorija, ubaciti instancu kasnjenja sa placanjem
+a zatim dodatne isplate naredni mesec u vidu duple uplate rate
+
+u trenutku kada zakasne sa placanjem, azurirati:
+
+credit_score oboriti za 5-10 (rand, u zavisnosti sa kasnjenjem)
+payment_amount 0
+payment_made 0
+dpd_current +30
+
+za sledeci mesec azurirati:
+payment made = 1
+payment amount = 2 * scheduled_emi
+credit_score podici za ~ 7 poena
+
+LOGIKA: 
+    razdvojiti 2. dataset po korisnicima, samplovati ~30% grupa
+    za svakog korisnika, izvuci jedan nasumican red i za njega odrediti da nisu platili ratu
+    za naredni mesec, realizvoati nadoknadu propusta
+    
+'''
+
+def late_payments(df):
+    
+    # razdvojiti 2. dataset po korisnicima, samplovati ~30% grupa
+    
+    unique_users = df_credit_score['customer_id'].unique()
+    sample_users = np.random.choice(unique_users, int(len(unique_users)/3))
+    
+    #za svakog korisnika, izvuci jedan nasumican red i za njega odrediti da nisu platili ratu
+    # specificno izostavljamo poslednja 4 meseca zbog ranijeg korigovanja za korisnike koji su likvidirani
+    
+    for user in sample_users:
+        rows = df_performance[df_performance['customer_id'] == user][:-4]
+
+        if (rows.empty):
+            continue
+
+        month_pick = np.random.choice(rows.index)
+        
+        # unosenje kasnjenja
+        credit_reduction = np.random.randint(5, 10)
+        
+        df.at[month_pick, 'payment_amount'] = 0
+        df.at[month_pick, 'payment_made'] = 0
+        df.at[month_pick, 'dpd_current'] = df.at[month_pick, 'dpd_current'] + 30
+        df.at[month_pick, 'credit_score'] = df.at[month_pick, 'credit_score'] - credit_reduction
+        
+        # peglanje vrednosti sledeci mesec
+        
+        df.at[month_pick + 1, 'payment_amount'] = df.at[month_pick + 1, 'scheduled_emi'] * 2 
+        df.at[month_pick + 1, 'payment_made'] = 1
+        df.at[month_pick + 1, 'dpd_current'] = 0
+        df.at[month_pick + 1, 'credit_score'] = df.at[month_pick, 'credit_score'] + credit_reduction*1.2
+        
+    return df
+
+
+#%% POSTAVLJANJE PUTANJA
+base_path = Path(__file__).parent / 'podaci/'
+
+path_credit = base_path / 'credit score/origination_data-2.csv'
+path_credit_tracking = base_path / 'credit score/monthly_performance-2.csv'
+
+#%% UCITAVANJE PODATAKA
+
+df_credit_score = pd.read_csv(path_credit)
+
+df_performance = pd.read_csv(path_credit_tracking)
+
+#%% UPOZNAVANJE SA PODACIMA
+
+initial_evaluation(df_credit_score) 
+
+initial_evaluation(df_performance) 
+
+#%% OSNOVNO SREDJIVANJE DPD
+
+print (np.unique(df_performance['dpd_current'])) 
+
+df_performance = backfill_dpd_progression(df_performance)
+
+
+print (np.unique(df_performance['dpd_current'])) 
+
+#%% DODATNO SREDJIVANJE DPD 
+'''
+rows = df_performance[df_performance['customer_id'] == 'CUST100007']
+
+print(rows[['dpd_current', 'payment_made', 'payment_amount']])
+
+df_performance = late_payments(df_performance)
+
+print(rows[['dpd_current', 'payment_made', 'payment_amount']])
+'''
+
+#%% UKLANJANJE 3M KOLONA
+    
+print (df_performance.columns)    
+
+df_performance.drop(['credit_score_delta_3m', 'revolving_util_delta_3m'], axis = 1, inplace = True)
+#%%KORELACIJE
+
+df_kljucna_polja = df_performance.drop(['customer_id', 'observation_month'], axis = 1)
+
+plot_correlation_for_col(df_kljucna_polja, 'has_defaulted')
 
 plot_correlation_heatmap(df_kljucna_polja)
 
+#%% PROVERA SELEKTIVNOSTI KATEGORICKIH KOLONA - Mainly zarad informacija za indeksiranja
+
+print (np.unique(df_credit_score['employment_status']))
+#tba
+
 #%% SKIP CELL, MONGODB WORK START
-#%% Saving records
+#%% Filling in collections
 
 from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017/")
 db = client["banking_db"]
 
+#%% Making collections
 db.origination_data.insert_many(
     df_credit_score.to_dict("records")
 )
@@ -205,38 +293,137 @@ db.monthly_performance.insert_many(
     df_performance.to_dict("records")
 )
 
-
-
-#%% example
-
-pipeline = [
-    {
-        "$match": {
-            "months_on_book": {"$lte": 12}
-        }
-    },
-    {
-        "$group": {
-            "_id": "$customer_id",
-            "avg_score": {
-                "$avg": "$credit_score"
-            }
-        }
-    }
-]
-
-stats = db.command(
-    "aggregate",
-    "monthly_performance",
-    pipeline=pipeline,
-    explain=True,
-    cursor={}
-)
-print(stats)
-
-
-
+print ("Insert complete!")
 
 #%%
+collection_og = db['origination_data']
+collection_performance = db['monthly_performance']
+#%% celija za proveravanje imena kolona datoteka
 
-print (np.count_nonzero(df_credit_score.loc['loan_term' == 36]))
+print(df_performance.columns)
+
+#%% OSNOVNI UPITI NAD NEOPTIMIZOVANOM SHEMOM
+#%% 1. Za svakog klijenta koji je ušao u default, odrediti mesec pre ulaska u 
+# default u kom je imao najveći pad kreditnog rejtinga u prethodna 3 meseca.
+
+# ovaj upit izvrsava se iskljucivo nad performance kolekcijom
+
+try:
+    pipeline_q1 = [
+        {
+            "$match" : {"has_defaulted" : 1}
+        },
+        {
+            "$setWindowFields" : {
+                "partitionBy" : "$customer_id",
+                "sortBy" : {"observation_month" : 1},
+                "output" : {"3m_frame" : {
+                    "$shift" : {
+                        "output": "$credit_score",
+                        "by": -3
+                        }
+                    }
+                }
+            }
+        },
+
+        {
+        "$addFields": {
+            "drop_3m": {
+                "$cond": [
+                    {"$ne": ["$3m_frame", None]},
+                    {"$subtract": ["$3m_frame", "$credit_score"]},
+                    None
+                    ]
+                }
+            }
+        },
+        {
+        "$match": {
+            "drop_3m": {"$ne": None}
+            }
+        },
+        {
+        "$group": {
+            "_id": "$customer_id",
+            "month_of_biggest_drop": {"$first": "$observation_month"},
+            "biggest_drop": {"$first": "$drop_3m"}
+            }
+        }
+    ]
+
+    results = collection_performance.aggregate(pipeline_q1)
+
+    for res in results:
+        print (res)
+
+except Exception as e:
+    raise Exception("An exception has occurred: ", e)
+
+#%% Running optimizer over query 1 version 1
+
+q1_v1_explain = db.command(
+    "explain",
+    {
+        "aggregate" : "monthly_performance",
+        "pipeline" : pipeline_q1, 
+        "cursor" : {}
+    },
+    verbosity = "executionStats"
+)
+
+print(q1_v1_explain)
+
+#%% 2. Pronaći klijente kod kojih je iskorišćenost kreditne kartice porasla više od 40%,
+#  a kreditna sposobnost opala za više od 100 poena u prethodna 3 meseca pre likvidacije.
+
+# za potrebe ovog upita, i dalje je dovoljan samo monthly performance
+
+# sta znaci window frame ovog upita:
+# self explanatory - pronalazimo mesec likvidacije
+# proveravamo razliku navedenih polja za [n-2]-[n-1] i [n-1]-[n]
+# imamo and uslov da su ispunjena oba kriterijuma
+# prvi put kada se ispune oba, uzimamo tog korisnika i nastavljamo dalje 
+
+# potencijalne optimizacije:
+# 1. sortiranje po has_defaulted, 0 unapred
+# 2. sortiranje po mesecu observacije, asc
+# 3. 
+
+# MOZEMO IZVUCI MESECE NA DRUGACIJI NACIN.
+# IZVUCI SAMO MESECE U KOJIMA JE DPD > 0 JER JE RASPODELA PODATAKA SADA TAKVA DA 
+# JEDINI PUT KADA DPD ZAPRAVO NIJE NULA JESTE U TRENUTKU KADA POSTANE 30 PA 60 PA 90 PRE NEMINOVNOG
+# DEFAULTA!
+
+# odavde proizilazi nova moguca optimizacija:
+# preuredjivanje dpd-a tako da se trenutci posle defaulta prebace na 120 i tako ostanu (u tranutnom datasetu su na 90)
+
+pipeline_q2 = [
+    {
+        "$match" : {"has_defaulted" : 1,
+                    "dpd_current" : "$gt 0"}
+    },
+    {
+        "$group" : {
+
+        }
+    }
+    {
+        "$setWindowFields" : {
+            "partitionBy" : "$customer_id",
+            "sortBy" : {"observation_month" : 1},
+            "output" : {
+                "default_month":{
+                    "$first" :"$observation_month"
+                    }
+                }
+            }
+    }
+    ,
+    {
+        "$project":
+        {"customer_id" : 1
+         }
+    }
+    ]
+# %%
